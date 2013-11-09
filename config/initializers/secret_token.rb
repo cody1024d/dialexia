39cb6768c64dcec4e6db7414f31e7357f0879561
Cody <cody1024d@gmail.com>
2013-11-08 18:35:22 -0600

    Initial commit of application for dialexia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dialexia::Application.config.secret_key_base = '1bcdd46cf2b01c8def24daf4f9c5fe2ed41a43d5320a78b26fe6da3dd690ca16a62eeb1caac85cc597b90c9c09f2fc1378b3679e5377f6862f3aeabb88873e40'
